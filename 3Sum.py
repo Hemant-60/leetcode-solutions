dbe90e68325d1d074f4a6ded2390a5c8c1872aab
Hemant Rana <38358739+Hemant-60@users.noreply.github.com>
2020-08-31 11:56:02 +0530

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -1,3 +1,9 @@
+'''
+Author : Hemant Rana
+Date : 31st Aug 2020
+link : https://leetcode.com/problems/3sum/
+'''
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
