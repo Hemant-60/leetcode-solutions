1625874d0d5147a6fbe4ee1185a96f6dbe32a5f7
Hemant Rana <38358739+Hemant-60@users.noreply.github.com>
2020-09-15 00:50:18 +0530

    Update sum-root-to-leaf-numbers.py

--- sum-root-to-leaf-numbers.py
+++ sum-root-to-leaf-numbers.py
@@ -4,18 +4,19 @@ https://leetcode.com/problems/sum-root-to-leaf-numbers/submissions/
 
 class Solution:
     s=0
+    tsum=0
     def paths(self, stack, root): 
         if root == None: 
             return
  
-        stack.append(root.val) 
+        stack.append(root.val)
+        self.tsum=self.tsum*10+root.val
         if(root.left == None and root.right == None): 
-            temp_sum=0
-            for i in stack:
-                temp_sum=temp_sum*10+i
-            self.s+=temp_sum
+            self.s+=self.tsum
         self.paths(stack, root.left) 
         self.paths(stack, root.right) 
+        self.tsum//=10
+        
         stack.pop() 
 
     def sumNumbers(self,root: TreeNode) -> int:
