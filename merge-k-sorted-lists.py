c73f2a409e722d71446608bfc98b7c1bfd91e244
Hemant Rana <38358739+Hemant-60@users.noreply.github.com>
2022-02-07 11:52:40 +0100

    Update merge-k-sorted-lists.py

--- merge-k-sorted-lists.py
+++ merge-k-sorted-lists.py
@@ -1,24 +1,35 @@
-'''
-https://leetcode.com/problems/merge-k-sorted-lists/
-'''
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        vals=[]
-        for head in lists:
-            while(head is not None):
-                vals.append(head.val)
-                head=head.next
-        vals.sort()
-        if vals==[]: return None
-        else:
-            head=ListNode(vals[0])
-            temp=head
-            for i in range(1,len(vals)):
-                temp.next=ListNode(vals[i])
-                temp=temp.next
-        return head
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        head = None
+        start = None
+        
+        Not_none_values = filter(None.__ne__, lists)
+        lists = list(Not_none_values)
+        
+        if lists == []: return None
+        # print(lists)
+        while lists:
+            index = -1
+            mn= 100000
+            for i in range(len(lists)):
+                if lists[i] is not None:
+                    if lists[i].val < mn:
+                        index = i
+                        mn = lists[i].val
+            if head is None:
+                head = ListNode(lists[index].val)
+                start = head
+            else:
+                head.next = ListNode(lists[index].val)
+                head = head.next
+            
+            lists[index] = lists[index].next
+            if lists[index] is None:
+                lists.pop(index)
+        
+        return start
